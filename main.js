baf000848fa4af25a7a410ba640b01ef7d4f5ba2
pam-29 <pamelaarijaona@gmail.com>
2026-02-04 12:58:53 +0100

    feat: change between two scenes

--- main.js
+++ main.js
@@ -1,136 +1,263 @@
 import * as THREE from 'three';
 import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitControls.js';
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color("#de6262");
+let camera;
+let scene;
+let renderer;
+let geometry;
+let material;
+let texture;
+let mesh;
 
-//camera initialisation
-const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set( 0, 0, 0.1 );
-
-//create a form
-const geometry = new THREE.SphereGeometry(50, 32, 32)
-const textureSalle = new THREE.TextureLoader().load('salle.jpg')
-const textureStage = new THREE.TextureLoader().load('stage.jpg')
-const textureEntrance = new THREE.TextureLoader().load('entrance.jpg')
-
-const material = new THREE.MeshBasicMaterial({
-    map: textureSalle,
-    side: THREE.DoubleSide
-})
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+let controls;
+let camera2;
+let scene1;
+let scene2;
+let geometry2;
+let material2;
+let texture2;
+let mesh2;
 
 
-const ambLight = new THREE.AmbientLight(0xFFFFFF);
-ambLight.position.set(0,0,0);
-ambLight.castShadow = false;
-scene.add(ambLight);
+let arrowForm;
+let arrowMaterial;
+let arrowRoom;
+let arrowStage;
+let arrowEntrance;
 
 
-const triForme = new THREE.ConeGeometry(1.5, -3, 3 )
-const triMat = new THREE.MeshBasicMaterial({ color: 0xf5f5f5 });
-const triangleSalle = new THREE.Mesh(triForme, triMat);
-const triangleStage = new THREE.Mesh(triForme, triMat);
-const triangleEntrance = new THREE.Mesh(triForme, triMat);
-
-
-//stage hotspot
-triangleStage.position.set(40, -10, 5);
-triangleStage.lookAt(0, 0, 0); 
-scene.add(triangleStage);
-
-//salle hotspot
-triangleSalle.position.set(0, -10, 0);
-triangleSalle.lookAt(0, 0, 0); 
-scene.add(triangleSalle);
-
-//entrance hotspot
-triangleEntrance.position.set(-40, -10, -5);
-triangleEntrance.lookAt(0, 0, 0); 
-scene.add(triangleEntrance);
-
-//renderer initialisation 
-const canvas = document.querySelector('canvas')
-console.log(canvas);
-
-const renderer = new THREE.WebGLRenderer({canvas})
-
-renderer.setSize(window.innerWidth,window.innerHeight)
-
-//orbitControls
-const controls = new OrbitControls(camera, canvas);
-controls.target.set(1, 0, 0);
-controls.enableDamping = true;
-controls.dampingFactor = 0.05;
-controls.rotateSpeed = -0.5;
-controls.enableZoom = false;
-controls.minPolarAngle = Math.PI / 2;
-controls.maxPolarAngle = Math.PI;
-
-function animate() {
-    requestAnimationFrame(animate)
-	controls.update();
-	renderer.render( scene, camera );
-}
-
+init();
 animate();
 
-const raycaster = new THREE.Raycaster();
-const mouse = new THREE.Vector2();
- 
+const firstScene = document.getElementById('scene1');
+
+/* Buttons to handle scene switch */
+firstScene.addEventListener('click', ()=>{
+    scene = scene1
+    console.log(scene);
+})
 
 
 
-// Go to room function
-window.addEventListener('click', (event) => {
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+function init() {
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera.position.set(0, 0, 0.1);
 
-    raycaster.setFromCamera(mouse, camera);
-    
-    const intersects = raycaster.intersectObjects([triangleStage, triangleSalle, triangleEntrance]);
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio( window.devicePixelRatio );
 
-    if (intersects.length > 0) {
-        const clickedObject = intersects[0].object;
+    document.body.appendChild(renderer.domElement);
 
-        if (clickedObject === triangleStage) goToRoom('stage');
-        if (clickedObject === triangleSalle) goToRoom('salle');
-        if (clickedObject === triangleEntrance) goToRoom('entrance');
-    }
-});
 
-triangleSalle.visible = false;
-triangleStage.visible = true;
-triangleEntrance.visible = true;
+    /*
+    *********************************************
+    Creation of the first scene
+    *********************************************
+    */
+    scene1 = new THREE.Scene();
+    geometry = new THREE.SphereGeometry(50, 32, 32);
+    texture = new THREE.TextureLoader().load('salle.jpg');
+    material = new THREE.MeshBasicMaterial({
+            map: texture,
+            side: THREE.DoubleSide
+        });
+    mesh = new THREE.Mesh(geometry, material);
+    scene1.add(mesh);
 
-function goToRoom(roomName) {
-    if (roomName === 'salle') {
-        material.map = textureSalle;
-        triangleStage.visible = true;
-        triangleEntrance.visible = true;
-        triangleSalle.visible = false;
-        controls.target.set(1, 0, 0);
-        triangleStage.position.set(40, -10, 5);
-        triangleEntrance.position.set(-40, -10, -5);
+    /*
+    *********************************************
+    Creation of the 360° image
+    *********************************************
+    */
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.target.set(1, -3, 0);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.05;
+    controls.rotateSpeed = -0.5;
+    controls.enableZoom = false;
+    controls.minPolarAngle = Math.PI / 2;
+    controls.maxPolarAngle = Math.PI;
+
+
+    /*
+    *********************************************
+    Creation of the arrows to mark 
+    *********************************************
+    */
+    arrowForm = new THREE.ConeGeometry(1.5, -3, 3 )
+    arrowMaterial = new THREE.MeshBasicMaterial({ color: 0xf5f5f5 });
+    arrowRoom = new THREE.Mesh(arrowForm, arrowMaterial);
+    arrowStage = new THREE.Mesh(arrowForm, arrowMaterial);
+    arrowEntrance = new THREE.Mesh(arrowForm, arrowMaterial);
+
+    //stage hotspot
+    arrowStage.position.set(40, -10, 5);
+    arrowStage.lookAt(0, 0, 0); 
+    scene1.add(arrowStage);
+
+    //salle hotspot
+    arrowRoom.position.set(0, -10, 0);
+    arrowRoom.lookAt(0, 0, 0); 
+    scene1.add(arrowRoom);
+
+    //entrance hotspot
+    arrowEntrance.position.set(-40, -10, -5);
+    arrowEntrance.lookAt(0, 0, 0); 
+    scene1.add(arrowEntrance);
+
+    const raycaster = new THREE.Raycaster();
+    const mouse = new THREE.Vector2();
+
+    window.addEventListener('click', (event) => {
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+        raycaster.setFromCamera(mouse, camera);
+        
+        const intersects = raycaster.intersectObjects([arrowStage, arrowRoom, arrowEntrance]);
+
+        if (intersects.length > 0) {
+            const clickedObject = intersects[0].object;
+
+            if (clickedObject === arrowStage){
+                goToRoom('stage');
+                console.log('tu es dans la salle');
+            } 
+            if (clickedObject === arrowRoom){
+                goToRoom('salle');
+            } 
+            if (clickedObject === arrowEntrance){
+                goToRoom('entrance');
+            } 
+        }
+    });
+
+
+    const textureSalle = new THREE.TextureLoader().load('salle.jpg')
+    const textureStage = new THREE.TextureLoader().load('stage.jpg')
+    const textureEntrance = new THREE.TextureLoader().load('entrance.jpg')
+
+
+    arrowRoom.visible = false;
+    arrowStage.visible = true;
+    arrowEntrance.visible = true;
+
+    function goToRoom(roomName) {
+        if (roomName === 'salle') {
+            material.map = textureSalle;
+            arrowStage.visible = true;
+            arrowEntrance.visible = true;
+            arrowRoom.visible = false;
+            controls.target.set(1, 0, 0);
+            arrowStage.position.set(40, -10, 5);
+            arrowEntrance.position.set(-40, -10, -5);
+            scene1.add( videoScreen );
+        } 
+        else if (roomName === 'stage') {
+            material.map = textureStage;
+            arrowStage.visible = false;
+            arrowEntrance.visible = false;
+            arrowRoom.visible = true;
+            arrowRoom.position.set(-40, -10, 0);
+            arrowRoom.lookAt(0, 0, 0); 
+            scene1.remove(videoScreen);
+        } 
+        else if (roomName === 'entrance') {
+            material.map = textureEntrance;
+            arrowStage.visible = false;
+            arrowEntrance.visible = false;
+            arrowRoom.visible = true;
+            arrowRoom.position.set(48, -8, 4);
+            arrowRoom.lookAt(0, 0, 0); 
+            scene1.remove(videoScreen);
+        }
+        
+        material.needsUpdate = true;
+        controls.update();
     } 
-    else if (roomName === 'stage') {
-        material.map = textureStage;
-        triangleStage.visible = false;
-        triangleEntrance.visible = false;
-        triangleSalle.visible = true;
-        triangleSalle.position.set(-40, -10, 0);
-        triangleSalle.lookAt(0, 0, 0);  
-    } 
-    else if (roomName === 'entrance') {
-        material.map = textureEntrance;
-        triangleStage.visible = false;
-        triangleEntrance.visible = false;
-        triangleSalle.visible = true;
-        triangleSalle.position.set(48, -8, 4);
-        triangleSalle.lookAt(0, 0, 0); 
-    }
+
+
+    /*
+    *********************************************
+    Implement the video
+    *********************************************
+    */
+    const video = document.getElementById( 'video' );
+    const videotexture = new THREE.VideoTexture( video );
+
+    texture.minFilter = THREE.LinearFilter;
+    texture.magFilter = THREE.LinearFilter;
+
+    let videoMaterial = new THREE.MeshBasicMaterial({
+        map: videotexture,
+        side: THREE.FrontSide,
+        toneMapped: false,
+    });
+
+    let videoForm = new THREE.BoxGeometry(15, 10, 1);
+    let videoScreen = new THREE.Mesh(videoForm, videoMaterial);
+    videoScreen.position.set(-10, 3, 20)
+    videoScreen.lookAt(-6, 5, 0); 
+    scene1.add( videoScreen );
+
+
+
+    /////////////////////////////////////////////////
+    //       Scene 2                               //
+    /////////////////////////////////////////////////
+    camera2 = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
+    camera2.position.z = 500;
+
+    scene2 = new THREE.Scene();
+
+    /*
+    *********************************************
+    When you click on the video in scene1, it leads you to the scene2
+    *********************************************
+    */
+    const raycaster1 = new THREE.Raycaster();
+    const mouse1 = new THREE.Vector2();
+
+
+    window.addEventListener('click', (e)=>{
+        mouse1.x = (e.clientX / window.innerWidth) * 2 - 1;
+        mouse1.y = -(e.clientY / window.innerHeight) * 2 + 1;
+
+        raycaster1.setFromCamera(mouse1, camera2);
+        
+        const intersects = raycaster.intersectObjects([videoScreen]);
+        console.log(intersects);
+
+        if (intersects.length > 0) {
+            scene = scene2
+            console.log(scene);
+        }
     
-    material.needsUpdate = true;
+    })
+
+
+    geometry2 = new THREE.BoxGeometry(100, 10, 10);
+    material2 = new THREE.MeshNormalMaterial();
+
+    mesh2 = new THREE.Mesh(geometry2, material2);
+    mesh2.position.set(0, 0, 150);
+    scene2.add(mesh2); // so note need to be able to switch this on 
+
+    // Choosing default scene as scene1
+    scene = scene1;
+}
+
+function animate() {
+    requestAnimationFrame(animate);
     controls.update();
+    render();
+}
+
+function render() {
+    if (scene == scene1){
+        renderer.render(scene, camera);
+    } else{
+        renderer.render(scene, camera2);
+    }   
 }
